d6518c928e2b9540a232151aeabfb29cac5beed0
jakedcolbert <jake.colbert@gordon.edu>
2022-04-23 19:41:13 -0400

    Jake's progress

--- p3/Twentivia/app/src/main/java/com/elijake/twentivia/Questions.kt
+++ p3/Twentivia/app/src/main/java/com/elijake/twentivia/Questions.kt
@@ -1,8 +1,22 @@
 package com.elijake.twentivia
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
-class Questions : AppCompatActivity() {
-    // API code goes in here
+class TriviaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide();
+        setContentView(R.layout.activity_questions)
+
+        val button = findViewById<Button>(R.id.button);
+
+        button.setOnClickListener {
+            val intent = Intent(this, Stats::class.java)
+            startActivity(intent)
+        }
+    }
 }
