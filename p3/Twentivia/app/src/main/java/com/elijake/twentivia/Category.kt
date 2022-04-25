6dd6967832c56ac2d761f5d5813f1329de3a0a10
Elijah Opoku-Nyarko <elijah.opoku-nyarko@gordon.edu>
2022-04-25 14:31:20 -0400

    Added functionality for the back button from the Category page to the Main page

--- p3/Twentivia/app/src/main/java/com/elijake/twentivia/Category.kt
+++ p3/Twentivia/app/src/main/java/com/elijake/twentivia/Category.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class Category : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +13,16 @@ class Category : AppCompatActivity() {
         supportActionBar?.hide();
 
         val startBtn: ImageButton = findViewById(R.id.startBtn);
+        val backButton: ImageView = findViewById(R.id.imageView4);
 
         startBtn.setOnClickListener {
             val intent: Intent = Intent(this, TriviaActivity::class.java);
             startActivity(intent);
+        }
+
+        backButton.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
         }
     }
 }
