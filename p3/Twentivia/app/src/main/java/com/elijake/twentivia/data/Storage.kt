97ade20de027c7a846bb2727459e78b00af3e942
jakedcolbert <jake.colbert@gordon.edu>
2022-05-06 09:21:21 -0400

    Progress

--- p3/Twentivia/app/src/main/java/com/elijake/twentivia/data/Storage.kt
+++ p3/Twentivia/app/src/main/java/com/elijake/twentivia/data/Storage.kt
@@ -4,11 +4,23 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.elijake.twentivia.model.Stat
+import kotlinx.coroutines.flow.*
 
-class Storage(val context: Context) {
+class Storage(private val context: Context) {
 
-    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "stats")
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "stats")
+
+    private suspend fun save(key: String, value: Stat) {
+        context.dataStore.edit { settings ->
+            settings[intPreferencesKey(key)] = value
+        }
+    }
+
+    private suspend fun read(key: String): Stat {
+        return context.dataStore.data.first()[intPreferencesKey(key)] as Stat
+    }
 
 }
